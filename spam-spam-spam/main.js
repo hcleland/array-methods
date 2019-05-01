51c1c5b063aa44508f3c188566c71248d93b6f6c
Heather Cleland <heathercleland@gmail.com>
2019-05-01 13:20:31 -0500

    complete spam, spam, spam exercise

---
+++ spam-spam-spam/main.js
@@ -0,0 +1,8 @@
+// From that array, extract just the customers' contact email addresses and store them in a new array.
+// You will need a nested array method - meaning one iteration inside another one -
+// since you need to iterate the entire array of customers,
+// and then iterate the array of emails for each one.
+
+console.log("I'm here with main");
+
+
